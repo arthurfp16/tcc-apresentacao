86d4ab1f7b6dd1b11d2c16605d21b01cf1b632a0
arthurfp16 <82186586+arthurfp16@users.noreply.github.com>
2022-06-22 18:03:18 -0300

    add code without react

---
+++ buttonWithoutReact/index.js
@@ -0,0 +1,6 @@
+var buttonText = 1 //State global
+
+function onButtonClick (element) {
+    element.innerText = buttonText
+    buttonText++
+}   
--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(1)
   return (
-    <button className='button' onClick={(e) => {
-      setCount(count + 1)
-      e.target.innerText = count
-    }}>
+    <button className='button'
+      onClick={
+        (event) => {
+          setCount(count + 1)
+          event.target.innerText = count
+        }
+      }
+    >
       Clique aqui!
     </button>
   )
