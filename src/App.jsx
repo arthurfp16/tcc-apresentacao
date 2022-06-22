629de54971eef067c6df081223c954491819e4d4
arthurfp16 <82186586+arthurfp16@users.noreply.github.com>
2022-06-22 17:55:46 -0300

    add code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(1)
+  return (
+    <button className='button' onClick={(e) => {
+      setCount(count + 1)
+      e.target.innerText = count
+    }}>
+      Clique aqui!
+    </button>
+  )
+}
+
+export default App
